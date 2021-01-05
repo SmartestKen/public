96093102d88bd10c376a7064e162f7fa4fbf5532
SmartestKen <k5shao@ucsd.edu>
2021-01-05 03:31:19 -0800

    from sync.sh

--- cdTimer.sh
+++ cdTimer.sh
@@ -13,8 +13,7 @@ do
 done
 # use ./cdTimer.sh hh:mm:ss
 
-    
-curEpoch=`date +%s`
+
 
 # default session 15 minutes
 # time=${1:-'0:15:00'}
@@ -38,37 +37,32 @@ trap "index=$((1-index))" SIGINT
 multiplier=1
 
 
-while true:
-
+while true
+do
     # elapse in seconds
     if [[ index -eq 0 ]]
     then
         time=600
+        type="(Resting)"
     else
         time=900
-
-    
+        type="(Working)"
+    fi
+        
+    curEpoch=`date +%s`
     endEpoch=$(($curEpoch+$time))
 
 
-
-while [ $curEpoch -lt $endEpoch ]
-do
-    echo -ne "\033[2K\r$((($endEpoch-$curEpoch)/60)) minutes left."
-    sleep 30
-    curEpoch=`date +%s`
+    while [ $curEpoch -lt $endEpoch ]
+    do
+        
+        echo -ne "\033[2K\r$((($endEpoch-$curEpoch)/60)) minutes left. $type"
+        sleep 30
+        curEpoch=`date +%s`
+    done
+    
+    # echo -ne "\033[2K\rTime's up"
 done
-
-
-echo -ne "\033[2K\rTime's up"
-
-
-
-
-
-
-
-
 
 
 # remember to pass arguments into the function
