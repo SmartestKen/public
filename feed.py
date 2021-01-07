de9702822533834265498f893708a2220a73fc5f
SmartestKen <k5shao@ucsd.edu>
2021-01-07 01:32:05 -0800

    from sync.sh

--- feed.py
+++ feed.py
@@ -34,7 +34,7 @@ else:
 
 
     with open('/home/private/daily_log', 'a') as f:
-        f.write("--------end of " + today_date + "--------\n")
+        f.write("-------- " + today_date + "--------\n")
 
 
     with open('/home/public/arxiv_feed', 'a') as f:
