aafbc4893c075163c26bea39e455bd1da8464fd7
SmartestKen <k5shao@ucsd.edu>
2021-01-14 14:07:42 -0800

    from sync.sh

--- feed.py
+++ feed.py
@@ -16,14 +16,13 @@ else:
 
 
     feed_list1 = ["math.AC", "math.AG", "math.CO", "math.RT", "math.AT",
-                  "math.CT", "math.RA", "math.QA", "math.KT", "math.GR"]
+                  "math.CT", "math.RA", "math.QA", "math.KT", "math.GR", "cs.MS", "cs.SC"]
     for category in feed_list1:
         feed = feedparser.parse("http://arxiv.org/rss/" + category).entries
         url_set1.update([x.id for x in feed])
 
     url_set2 = set()
-    feed_list2 = ["cs.LG", "cs.AI", "stat.ML", "math.OC", "cs.CV",
-                  "cs.MS", "cs.NE", "cs.SC", "eess.SY"]
+    feed_list2 = ["cs.LG", "cs.AI", "stat.ML", "math.OC"]
     for category in feed_list2:
         feed = feedparser.parse("http://arxiv.org/rss/" + category).entries
         url_set2.update([x.id for x in feed])
