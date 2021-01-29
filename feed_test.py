f81c517660efbdf8e72e1061a60e7326055f3444
SmartestKen <k5shao@ucsd.edu>
2021-01-28 23:27:09 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -1,22 +1,5 @@
+import urllib
 
-import feedparser
-url_set1 = set()
-feed_list1 = ["math.AC"]
-for category in feed_list1:
-    feed = feedparser.parse("http://arxiv.org/rss/" + category)
-    print(feed)
-
-    exit(0)
-    url_set1.update([x.id for x in feed])
-
-url_set = set()
-feed = feedparser.parse("http://arxiv.org/rss/" + "math.OC").entries
-url_set.update([[x.id, x.title] for x in feed])
-
-url_set.update(url_set1)
-print(url_set)
-
-
-
-
-
+url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
+data = urllib.urlopen(url).read()
+print(data)
