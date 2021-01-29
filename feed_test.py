a6bec0adebe6e7e9558c03d159e5a5a3f42bfb50
SmartestKen <k5shao@ucsd.edu>
2021-01-28 20:08:33 -0800

    from sync.sh

---
+++ feed_test.py
@@ -0,0 +1,19 @@
+
+import feedparser
+url_set1 = set()
+feed_list1 = ["math.AC"]
+for category in feed_list1:
+    feed = feedparser.parse("http://arxiv.org/rss/" + category).entries
+    url_set1.update([x.id for x in feed])
+
+url_set = set()
+feed = feedparser.parse("http://arxiv.org/rss/" + "math.OC").entries
+url_set.update([x.id for x in feed])
+
+url_set.update(url_set1)
+print(url_set)
+
+
+
+
+
