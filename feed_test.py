d29d16a4e73ad29869b343453975f0d56180457c
SmartestKen <k5shao@ucsd.edu>
2021-01-29 12:56:15 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -1,7 +1,7 @@
 import urllib.request
 import feedparser
 
-url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
+url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=100'
 data = urllib.request.urlopen(url).read()
 feed = feedparser.parse(data)
 for entry in feed.entries:
