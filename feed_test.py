b900a8d8d7407b699bd39614e1dbdbc4bd67b2e8
SmartestKen <k5shao@ucsd.edu>
2021-01-29 22:15:57 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -1,23 +1,20 @@
 import urllib.request
 import feedparser
-'''
-url = 'http://export.arxiv.org/api/query?search_query=all:cs.LG&start=0&max_results=100&sortBy=submittedDate&sortOrder=descending'
+
+url = 'http://export.arxiv.org/api/query?search_query=all:math.OC&start=0&max_results=100&sortBy=submittedDate&sortOrder=descending'
 data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
 feed = feedparser.parse(data)
-count = 0
+
 for entry in feed.entries:
-    print(entry.id)
-    print(entry.title)
-    count += 1
-print(count)
-
-'''
-url = "https://scholar.google.com/scholar?q=Sequential+Quadratic+Programming+Methods."
-data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
-index = data.find(b'Cited by ') + 9
-if index != 8:
-    print(''.join(filter(str.isdigit, str(data[index:index+7]))))
-
+    # print(entry.id)
+    # print(entry.title)
+    url = "https://scholar.google.com/scholar?q=" + entry.title.replace(" ", "+")
+    data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
+    index = data.find(b'Cited by ') + 9
+    if index != 8:
+        print(entry.title)
+        print(''.join(filter(str.isdigit, str(data[index:index+7]))))
+    sleep(1)
 
 
 '''
