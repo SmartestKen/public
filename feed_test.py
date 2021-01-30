1a0fdb41e04526a733587b655f5f51cdac26dd04
SmartestKen <k5shao@ucsd.edu>
2021-01-30 11:44:01 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -1,7 +1,7 @@
 import urllib.request
 from xml.etree import ElementTree
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 # url = 'http://export.arxiv.org/api/query?search_query=all:math.OC&start=0&max_results=100&sortBy=submittedDate&sortOrder=descending'
 # find a previous date from counter
@@ -13,6 +13,10 @@ data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent':
 prefix = '{http://www.openarchives.org/OAI/2.0/}'
 prefix2 = '{http://arxiv.org/OAI/arXivRaw/}'
 prefix3 = "arxiv.org/abs/"
+driver = webdriver.Chrome("/usr/bin/brave-browser-stable")
+options = Options()
+options.headless = True
+
 for child in ElementTree.fromstring(data).find(prefix + "ListRecords"):
     info = child.find(prefix+"metadata").find(prefix2+"arXivRaw")
     print(info.find(prefix2+"categories").text)
