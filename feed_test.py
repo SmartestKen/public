e560933e189f710acf5b2cb11a71e708dd70ea21
SmartestKen <k5shao@ucsd.edu>
2021-01-30 03:11:25 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -2,17 +2,21 @@ import urllib.request
 from xml.etree import ElementTree
 
 # url = 'http://export.arxiv.org/api/query?search_query=all:math.OC&start=0&max_results=100&sortBy=submittedDate&sortOrder=descending'
+# date rotation process
 url = "http://export.arxiv.org/oai2?verb=ListRecords&set=math&from=2021-01-29&metadataPrefix=arXivRaw"
 data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
-print(data)
-root = ElementTree.fromstring(data)
+
+
+
 prefix = '{http://www.openarchives.org/OAI/2.0/}'
 prefix2 = '{http://arxiv.org/OAI/arXivRaw/}'
-for child in root.find(prefix + "ListRecords"):
+prefix3 = "arxiv.org/abs/"
+for child in ElementTree.fromstring(data).find(prefix + "ListRecords"):
     info = child.find(prefix+"metadata").find(prefix2+"arXivRaw")
     print(info.find(prefix2+"categories").text)
 
-    print(info.find(prefix2 + "id").text)
+    # make a feasible filter
+    print(prefix3 + info.find(prefix2 + "id").text)
     print(info.find(prefix2+"title").text)
 
     # print(child.find(prefix+"metadata").find(prefix+"arxivRaw").find(prefix+"categories").text)
