44db4588ae293174cb6e3a6005ae5e1a67972a94
SmartestKen <k5shao@ucsd.edu>
2021-01-29 22:31:17 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -1,5 +1,6 @@
 import urllib.request
 import feedparser
+import time
 
 url = 'http://export.arxiv.org/api/query?search_query=all:math.OC&start=0&max_results=100&sortBy=submittedDate&sortOrder=descending'
 data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
@@ -8,13 +9,13 @@ feed = feedparser.parse(data)
 for entry in feed.entries:
     # print(entry.id)
     # print(entry.title)
-    url = "https://scholar.google.com/scholar?q=" + entry.title.replace(" ", "+")
+    url = "https://scholar.google.com/scholar?q=" + entry.title.replace(" ", "+").replace(
     data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
     index = data.find(b'Cited by ') + 9
     if index != 8:
         print(entry.title)
         print(''.join(filter(str.isdigit, str(data[index:index+7]))))
-    sleep(1)
+    time.sleep(1)
 
 
 '''
