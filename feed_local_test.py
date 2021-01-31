f92af5e65902db1347f16b5b0dcfdbaf5d3eb52a
SmartestKen <k5shao@ucsd.edu>
2021-01-30 20:03:31 -0800

    from sync.sh

--- feed_local_test.py
+++ feed_local_test.py
@@ -1,11 +1,14 @@
 import json
 
+feed_set1 = {"math.AC", "math.AG", "math.AT"}
+feed_set2 = {"math.OC"}
+accept_set = set()
 with open("/home/k5shao/Downloads/arxiv-meta.json/arxiv-meta.json", "r") as f:
     for line in f:
-        print(line)
         a = json.loads(line)
-        print(a["id"])
-        print(a.keys())
-        break
+        print(a["id"], a["title"], a["categories"])
 
+        if len(a["categories"].split()) >= 2:
+            break
+        # id' 'title' 'categories'
 
