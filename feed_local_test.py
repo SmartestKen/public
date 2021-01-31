bf29e86bc4262a7b7f5b9e73d14bc2f04c28a2b6
SmartestKen <k5shao@ucsd.edu>
2021-01-30 19:48:10 -0800

    from sync.sh

--- feed_local_test.py
+++ feed_local_test.py
@@ -1,7 +1,10 @@
+import json
+
 with open("/home/k5shao/Downloads/arxiv-meta.json/arxiv-meta.json", "r") as f:
     for line in f:
         print(line)
+        a = json.loads(line)
+        print(a["id"])
         break
 
 
-        % load line????
