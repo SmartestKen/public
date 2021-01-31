fd999764c895f7f49658106c5b4c8c3ab458a8be
SmartestKen <k5shao@ucsd.edu>
2021-01-30 20:08:37 -0800

    from sync.sh

--- feed_local_test.py
+++ feed_local_test.py
@@ -3,12 +3,12 @@ import json
 feed_set1 = {"math.AC", "math.AG", "math.AT"}
 feed_set2 = {"math.OC"}
 accept_set = set()
+id_prefix = "https://arxiv.org/abs/"
 with open("/home/k5shao/Downloads/arxiv-meta.json/arxiv-meta.json", "r") as f:
     for line in f:
         a = json.loads(line)
-        print(a["id"], a["title"], a["categories"])
-
-        if len(a["categories"].split()) >= 2:
-            break
-        # id' 'title' 'categories'
+        art_set = set([x.strip() for x in a["categories"].split()])
+        if bool(given_set & feed_set1) and bool(given_set & feed_set2):
+            accept_set.add(id_prefix + a["id"] + "\n"
+                           + a["title"] + "\n")
 
