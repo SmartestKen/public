7e03762c405ae87048c788751054af70c37b8715
SmartestKen <k5shao@ucsd.edu>
2021-01-30 20:13:44 -0800

    from sync.sh

--- feed_local_test.py
+++ feed_local_test.py
@@ -8,7 +8,11 @@ with open("/home/k5shao/Downloads/arxiv-meta.json/arxiv-meta.json", "r") as f:
     for line in f:
         a = json.loads(line)
         art_set = set([x.strip() for x in a["categories"].split()])
-        if bool(given_set & feed_set1) and bool(given_set & feed_set2):
+        if bool(art_set & feed_set1) and bool(art_set & feed_set2):
+            print(a["id"], a["title"])
             accept_set.add(id_prefix + a["id"] + "\n"
                            + a["title"] + "\n")
 
+with open('/home/public/arxiv_collection2', 'w') as f:
+    for item in list(accept_set):
+        f.write("%s" % item)
