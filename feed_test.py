0f897b5965476a8e26cdcdbac776401a90fbe21f
SmartestKen <k5shao@ucsd.edu>
2021-01-30 16:29:15 -0800

    from sync.sh

--- feed_test.py
+++ feed_test.py
@@ -8,6 +8,10 @@ date = (datetime.today() - timedelta(days=10)).strftime('%Y-%m-%d')
 url = "http://export.arxiv.org/oai2?verb=ListRecords&set=math&from="+date+"&until="+date+"&metadataPrefix=arXivRaw"
 data = urllib.request.urlopen(urllib.request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
 
+feed_set1 = set("math.AC", "math.AG", "math.AT")
+feed_list2 =
+
+
 
 
 prefix = '{http://www.openarchives.org/OAI/2.0/}'
@@ -17,8 +21,9 @@ prefix3 = "https://arxiv.org/abs/"
 
 for child in ElementTree.fromstring(data).find(prefix + "ListRecords"):
     info = child.find(prefix+"metadata").find(prefix2+"arXivRaw")
-    print(info.find(prefix2+"categories").text)
 
+
+    print(info.find(prefix2 + "categories").text.split())
     # make a feasible filter
     print(prefix3 + info.find(prefix2 + "id").text)
     print(info.find(prefix2+"title").text)
